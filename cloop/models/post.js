ccd8858ae4009e09b49ae31067e67483e9e0cd42
Danny Tang <data1013@mit.edu>
2016-11-18 22:28:13 -0500

    schemas for post and comments

--- cloop/models/post.js
+++ cloop/models/post.js
@@ -1,3 +1,23 @@
 var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
+var PostSchema = mongoose.Schema({
+    author: {type: ObjectId, ref:"User"}],
+    text: String,
+    resource: {type: String, default: null},
+    comments: [{type: ObjectId, ref:"Comment"}],
+    timeCreated: {type: Date, default: Date.now},
+    timeEdited: {type: Date, default: null},
+    removed: [type: Boolean, default: false],
+    numUpvotes: {type: Number, default: 0},
+    numFlags: {type: Number, default: 0}
+});
+
+//TODO: Methods
+
+
+
+
+var PostModel = mongoose.model("Post", PostSchema);
+
+module.exports = PostModel;
