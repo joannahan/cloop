619f7b7d59ba805206d3be6319e4fce54ee1d6ac
Joanna Han <jmhan@mit.edu>
2016-11-18 22:29:50 -0500

    schemas for class and user

--- cloop/models/class.js
+++ cloop/models/class.js
@@ -1,3 +1,19 @@
 var mongoose = require("mongoose");
-var ObjectId = mongoose.Schema.Types.ObjectId;
+var User = require("./user.js");
+
+var ClassSchema = mongoose.Schema({
+	  name:{
+		  type:String,
+		  index:true,
+		  required:true
+	  },
+	  students:[{
+		  type: mongoose.Schema.Types.ObjectId, 
+		  ref: 'User'
+	  }]
+	});
+
+var Class = module.exports = mongoose.model("Class", ClassSchema);
+
+//TODO: methods
 
--- cloop/models/user.js
+++ cloop/models/user.js
@@ -1,3 +1,48 @@
 var mongoose = require("mongoose");
-var ObjectId = mongoose.Schema.Types.ObjectId;
+var Class = require("./class.js");
+var Post = require("./post.js");
+var Comment = require("./comment.js");
 
+var UserSchema = mongoose.Schema({
+  kerberos:{
+	  type:String,
+	  index:true,
+	  required:true
+  },
+  name:{
+	  type:String
+  },
+  classesTaken:[{
+	  type: mongoose.Schema.Types.ObjectId, 
+	  ref: 'Class'
+  }], 
+  classesEnrolled:[{
+	  type: mongoose.Schema.Types.ObjectId, 
+	  ref: 'Class'
+  }], 
+  //TODO: find a better way to do this vvvvvvv
+  upvotePost:[{
+	  type: mongoose.Schema.Types.ObjectId, 
+	  ref: 'Post'
+  }],
+  upvoteComment:[{
+	  type: mongoose.Schema.Types.ObjectId, 
+	  ref: 'Comment'
+  }],
+  flagPost:[{
+	  type: mongoose.Schema.Types.ObjectId, 
+	  ref: 'Post'
+  }], 
+  flagComment:[{
+	  type: mongoose.Schema.Types.ObjectId, 
+	  ref: 'Comment'
+  }], 
+  flaggedRemovals:{
+	  type:Number,
+	  default: 0
+  }
+});
+
+var User = module.exports = mongoose.model("User", UserSchema);
+
+//TODO: methods
