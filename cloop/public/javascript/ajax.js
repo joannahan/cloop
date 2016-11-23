cf3e97d30c36353d4ab9384a66f3fbec62341a51
Danny Tang <data1013@mit.edu>
2016-11-22 22:51:10 -0500

    upvote and flag ajax

---
+++ cloop/public/javascript/ajax.js
@@ -0,0 +1,53 @@
+$(document).ready(function() { 
+
+	$(".upvote.post").click(function() {
+		var postId = $(this).parent().eq(3).attr("id");
+		var upvoteSpan = $(this).parent();
+		var numCurrentUpvotes = upvoteSpan.text();
+		numCurrentUpvotes = parseInt(numCurrentUpvotes) + 1;
+
+		$.ajax({
+			url: '/post/upvote',
+			data: {
+				postId: postId
+			},
+			type: 'POST',
+			success: function(data){
+				if (data === "Success!") {
+					upvoteSpan.text() = numCurrentUpvotes;
+				} else {
+					console.log(data);
+				}
+			},
+			error: function(xhr, status, error) {
+				console.log("A problem occurred" + error);
+			}
+		});
+	});
+
+	$(".upvote.comment").click(function() {
+		var commentId = $(this).parent().eq(3).attr("id");
+		var upvoteSpan = $(this).parent();
+		var numCurrentUpvotes = upvoteSpan.text();
+		numCurrentUpvotes = parseInt(numCurrentUpvotes) + 1;
+
+		$.ajax({
+			url: '/comment/upvote',
+			data: {
+				commentId: commentId
+			},
+			type: 'POST',
+			success: function(data){
+				if (data === "Success!") {
+					upvoteSpan.text() = numCurrentUpvotes;
+				} else {
+					console.log(data);
+				}
+			},
+			error: function(xhr, status, error) {
+				console.log("A problem occurred" + error);
+			}
+		});
+	});
+
+}
